80ceb115bd15db38f4ae4630ceb5c3a84a0cede9
adrielly1 <143034762+adrielly1@users.noreply.github.com>
2024-04-01 14:16:57 -0300

    Sedans.jsx

--- src/components/Sedans.jsx
+++ src/components/Sedans.jsx
@@ -5,7 +5,7 @@ export default function Sedans() {
         <section className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
           <img src={iconSedans} alt="icone dos carros sedan" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
-          <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <p className="font-lexend mb-7 text-offwhitebg lg:mb-20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans"
           target="_blank" className="font-lexend bg-offwhitebg px-10 py-3 block w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-orange hover:text-offwhitebg">Ver mais</a>
         </section>
