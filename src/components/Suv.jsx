d76c31e63ec35bc403dc2337fa7146816814d661
adrielly1 <143034762+adrielly1@users.noreply.github.com>
2024-04-01 14:17:20 -0300

    Suv.jsx

--- src/components/Suv.jsx
+++ src/components/Suv.jsx
@@ -5,7 +5,7 @@ export default function Suv() {
         <section className="bg-green p-12">
           <img src={iconSuvs} alt="icone dos carros SUV" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">SUVs</h2>
-          <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <p className="font-lexend mb-7 text-offwhitebg lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/suvs"
           target="_blank" className="font-lexend bg-offwhitebg px-10 py-3 block w-fit rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg">Ver mais</a>
         </section>
